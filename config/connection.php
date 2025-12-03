0fe5848ddef4559bcaffdf82d92ad3dbe911c977
andizaid <andizaid27@gmail.com>
2025-12-03 10:27:47 +0800

    add connection

---
+++ config/connection.php
@@ -0,0 +1,14 @@
+<?php 
+$server = "localhost";
+$user = "root";
+$pass = "";
+$database = "sibal_db";
+
+$conn = mysqli_connect($server, $user, $pass, $database);
+
+if(!$conn){
+    die("Connection failed: " . mysqli_connect_error());
+}else{
+    echo "Connected successfully";
+}
+?>
