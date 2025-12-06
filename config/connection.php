5d93eea7e6b11924293ff11fe753e0be7fbdc6e3
andizaid <andizaid27@gmail.com>
2025-12-06 12:58:48 +0800

    change conn

--- config/connection.php
+++ config/connection.php
@@ -1,7 +1,7 @@
 <?php 
 $server = "localhost";
 $user = "turh8536_sibal_user";
-$pass = "SibalUINAM2025";
+$pass = "SibalFix2025!";
 $database = "turh8536_sibal_db";
 
 $conn = mysqli_connect($server, $user, $pass, $database);
