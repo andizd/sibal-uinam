13902aa85ff4b4684fa5ba9ce1c5bc47375c082f
andizaid <andizaid27@gmail.com>
2025-12-07 12:09:52 +0800

    F

--- index.php
+++ index.php
@@ -126,6 +126,9 @@ $result = mysqli_query($conn, $query);
                                         class="bg-yellow-400 hover:bg-yellow-500 text-white p-2 rounded-md shadow">
                                         <i class="fas fa-edit"></i>
                                     </a>
+                                    <a onclick="return confirm('Yakin ingin menghapus data ini?')" href="process/delete_process.php?id=<?= $row['id_barang']; ?>" class="bg-red-500 hover:bg-red-600 text-white p-2 rounded-md shadow transition" title="Hapus">
+                                        <i class="fas fa-trash"></i>
+                                    </a>
 
                                 <?php else: ?>
 
@@ -137,9 +140,6 @@ $result = mysqli_query($conn, $query);
                                     </a>
 
                                 <?php endif; ?>
-                                <a onclick="return confirm('Yakin ingin menghapus data ini?')" href="process/delete_process.php?id=<?= $row['id_barang']; ?>" class="bg-red-500 hover:bg-red-600 text-white p-2 rounded-md shadow transition" title="Hapus">
-                                    <i class="fas fa-trash"></i>
-                                </a>
                             </div>
                         </td>
                     </tr>
